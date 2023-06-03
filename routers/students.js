01d95f960bcaaff6ca81e2bc0c0a9450437482f1
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-03 23:17:24 +0530

    Update. Updated routers using MVC architecture.

--- routers/students.js
+++ routers/students.js
@@ -1,82 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const studentModel = require('../models/students')
+const {getAllStudents, addAStudent, getAStudent, updateAStudent, deleteAStudent, getStudent } = require('../controllers/students')
 
-router.get('/',async (request,response)=>{
-    const students = await studentModel.find();
-    try{
-        response.status(200).json(students)
-    }
-    catch(error){
-        response.send(500).json({message : error.message})
-    }
-})
+// db is external dependency so we use async
 
-router.post('/', async (request,response)=>{
-    const newStudent = new studentModel({
-        name : request.body.name,
-        enrollDepartment :request.body.enrollDepartment,
-        enrollmentDate : request.body.enrollmentDate
-    })
-    try{
-        const student = await newStudent.save()
-        response.json(student)
-    }
-    catch(error)
-    {
-        response.json({errorMessage : error.message})
-    }
-    
-})
+router.route('/').get(getAllStudents).post(addAStudent)
 
-router.get('/:id', getStudent, (request,response)=>{
-    response.status(200).json(response.student)
-} )
+router.route('/:id').get(getStudent, getAStudent).patch(getStudent, updateAStudent).delete(getStudent, deleteAStudent)
 
-router.patch('/:id', getStudent, async(request,response)=>{
-    if(request.body.name != null)
-    {
-        response.student.name = request.body.name
-    }
-
-    if(request.body.enrollDepartment != null)
-    {
-        response.student.enrollDepartment = request.body.enrollDepartment
-    }
-
-    try{
-        const updatedStudent = await response.student.save()
-        response.status(200).json(updatedStudent)
-    }
-    catch(error){
-        response.status(500).json({message : error.message})
-    }
-})
-
-router.delete('/:id', getStudent, async(request,response)=>{
-    try{
-        await response.student.deleteOne();
-        response.json({message : `Deleted the user ${response.student.name}`}) // why not status ???
-    }
-    catch(error){
-        response.status(500).json({message : error.message})
-    }
-})
-
-async function getStudent(request,response,next){
-    let student
-    try{
-        student = await studentModel.findById(request.params.id)
-        if(student == null)
-        {
-            return response.status(500).json({message: error.message})
-        }
-    }
-    catch(error){
-        return response.status(500).json({message : error.message})
-    }
-    response.student = student
-    next()
-}
 
 module.exports = router
